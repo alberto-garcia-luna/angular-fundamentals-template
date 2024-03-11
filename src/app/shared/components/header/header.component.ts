f89a32582a9a82531438cad16e16bd9831ed7fba
Alberto Garcia Luna <Alberto.GarciaLuna@lseg.com>
2024-03-11 10:25:37 -0600

    Module 2 - Fix header

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,9 +6,4 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-  @Output() login = new EventEmitter();
-
-  loginButtonClick() {
-    this.login.emit();
-  }  
 }
