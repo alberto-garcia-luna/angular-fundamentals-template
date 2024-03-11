7e9009f73ed2e2c6892598d400e247b4673ea8fd
Alberto Garcia Luna <Alberto.GarciaLuna@lseg.com>
2024-03-11 08:08:21 -0600

    Fix Build

--- src/app/shared/directives/email.directive.ts
+++ src/app/shared/directives/email.directive.ts
@@ -1,16 +1,9 @@
 import { Directive } from "@angular/core";
-import { AbstractControl, ValidationErrors, Validator } from "@angular/forms";
 
 @Directive({
     selector: '[emailValidator]',
     providers: [/*Add your code here*/]
 })
-export class EmailValidatorDirective implements Validator {
-    validate(control: AbstractControl<any, any>): ValidationErrors | null {
-        throw new Error("Method not implemented.");
-    }
-    registerOnValidatorChange?(fn: () => void): void {
-        throw new Error("Method not implemented.");
-    }
+export class EmailValidatorDirective {
     // Add your code here
 }
