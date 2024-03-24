931fded58d37d944ea1607dc943ce491eb7cfada
Alberto Garcia Luna <alberto_garcia_luna@epam.com>
2024-03-23 20:11:23 -0600

    Module 4 - Fix routing file with all routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,27 @@ import { Routes, RouterModule } from '@angular/router';
 export const routes: Routes = [
     /* Add your code here */
     {
+        path: 'login', 
+        loadChildren: () => import('./shared/shared.module').then(module => module.SharedModule)
+    },
+    {
+        path: 'registration', 
+        loadChildren: () => import('./shared/shared.module').then(module => module.SharedModule)
+    },
+    {
         path: 'courses', 
+        loadChildren: () => import('./features/courses/courses.module').then(module => module.CoursesModule) 
+    },
+    {
+        path: 'courses/add', 
+        loadChildren: () => import('./features/courses/courses.module').then(module => module.CoursesModule) 
+    },
+    {
+        path: 'courses/edit/:id', 
+        loadChildren: () => import('./features/courses/courses.module').then(module => module.CoursesModule) 
+    },
+    {
+        path: 'courses/:id',
         loadChildren: () => import('./features/courses/courses.module').then(module => module.CoursesModule) 
     },
     { path: '', redirectTo: 'courses', pathMatch: 'full' },
