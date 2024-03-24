294bf91b5e417ace2911a676f8c7aab56a9a3389
Alberto Garcia Luna <alberto_garcia_luna@epam.com>
2024-03-23 20:00:15 -0600

    Module 4 - Fix routing file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,6 @@ import { Routes, RouterModule } from '@angular/router';
 export const routes: Routes = [
     /* Add your code here */
     {
-        path: 'login', 
-        loadChildren: () => import('./shared/shared.module').then(module => module.SharedModule)
-    },
-    {
         path: 'courses', 
         loadChildren: () => import('./features/courses/courses.module').then(module => module.CoursesModule) 
     },
